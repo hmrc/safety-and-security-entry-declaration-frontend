583ea616066ca37377dc9adf7142973b3003c013
Juan Antonio Rodríguez Pozo <versatil5634@gmail.com>
2022-02-24 10:41:53 +0000

    adding  page

---
+++ app/controllers/AddGoodsController.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions._
+import forms.AddGoodsFormProvider
+import models.{Index, LocalReferenceNumber, Mode}
+import pages.AddGoodsPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import viewmodels.checkAnswers.GoodsSummary
+import views.html.{AddDocumentView, AddGoodsView}
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class AddGoodsController @Inject()(
+  override val messagesApi: MessagesApi,
+  sessionRepository: SessionRepository,
+  identify: IdentifierAction,
+  getData: DataRetrievalActionProvider,
+  requireData: DataRequiredAction,
+  formProvider: AddGoodsFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  view: AddGoodsView
+)(implicit ec: ExecutionContext)
+  extends FrontendBaseController
+  with I18nSupport {
+
+  val form = formProvider()
+
+  def onPageLoad(mode: Mode, lrn: LocalReferenceNumber): Action[AnyContent] =
+    (identify andThen getData(lrn) andThen requireData) { implicit request =>
+
+      val goods = GoodsSummary.rows(request.userAnswers)
+
+      Ok(view(form, mode, lrn, goods))
+    }
+
+  def onSubmit(mode: Mode, lrn: LocalReferenceNumber): Action[AnyContent] =
+    (identify andThen getData(lrn) andThen requireData) { implicit request =>
+
+      form
+        .bindFromRequest()
+        .fold(
+          formWithErrors => {
+            val goods = GoodsSummary.rows(request.userAnswers)
+
+            BadRequest(view(formWithErrors, mode, lrn, goods))
+          },
+          value =>
+            Redirect(AddGoodsPage().navigate(mode, request.userAnswers, value))
+        )
+    }
+}
---
+++ app/pages/AddGoodsPage.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pages
+
+import controllers.routes
+import models.{CheckMode, Index, Mode, NormalMode, UserAnswers}
+import play.api.mvc.Call
+import queries.{DeriveNumberOfDocuments, DeriveNumberOfGoods}
+
+final case class AddGoodsPage() extends Page {
+
+  def navigate(mode: Mode, answers: UserAnswers, addAnother: Boolean): Call =
+    if (addAnother) {
+      answers.get(DeriveNumberOfGoods()) match {
+        case Some(size) =>
+          routes.CommodityCodeKnownController.onPageLoad(mode, answers.lrn, Index(size))
+        case None => routes.JourneyRecoveryController.onPageLoad()
+      }
+    } else {
+      mode match {
+        case NormalMode =>
+          routes.DangerousGoodController.onPageLoad(NormalMode, answers.lrn, Index(0))
+        case CheckMode => routes.CheckYourAnswersController.onPageLoad(answers.lrn)
+      }
+    }
+}
