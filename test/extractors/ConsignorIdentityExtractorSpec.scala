009067bfcf5b84cab947836a1616525ed0ac1b4a
Adam Cooper <61467301+AdamCoops@users.noreply.github.com>
2022-03-10 18:01:21 +0000

    decrease index(x) counts

--- test/extractors/ConsignorIdentityExtractorSpec.scala
+++ test/extractors/ConsignorIdentityExtractorSpec.scala
@@ -27,9 +27,9 @@ import pages.consignors._
 
 class ConsignorIdentityExtractorSpec extends SpecBase {
   private val baseAnswers: UserAnswers = arbitrary[UserAnswers].sample.value
-  private val itemOne: Index = Index(1)
-  private val itemTwo: Index = Index(2)
-  private val itemThree: Index = Index(3)
+  private val itemOne: Index = Index(0)
+  private val itemTwo: Index = Index(1)
+  private val itemThree: Index = Index(2)
 
   private val name = "Test Name"
   private val gb = Country("GB", "United Kingdom")
