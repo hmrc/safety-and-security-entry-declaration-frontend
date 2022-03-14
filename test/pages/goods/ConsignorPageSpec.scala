8045a59fe766f418d48d03b64570b2f9ffa34ffa
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-03-14 15:42:14 +0000

    SSUT-290: Navigate from Consignor page

--- test/pages/goods/ConsignorPageSpec.scala
+++ test/pages/goods/ConsignorPageSpec.scala
@@ -17,11 +17,15 @@
 package pages.goods
 
 import base.SpecBase
+import controllers.goods.{routes => goodsRoutes}
 import controllers.routes
-import models.{CheckMode, NormalMode}
+import models.{CheckMode, GbEori, Index, NormalMode}
+import org.scalacheck.Arbitrary.arbitrary
 import pages.behaviours.PageBehaviours
+import pages.consignees.{ConsigneeEORIPage, NotifiedPartyEORIPage}
+import queries.consignees.{ConsigneeKeyQuery, NotifiedPartyKeyQuery}
 
-class ConsignorSpec extends SpecBase with PageBehaviours {
+class ConsignorPageSpec extends SpecBase with PageBehaviours {
 
   "ConsignorPage" - {
 
@@ -31,12 +35,127 @@ class ConsignorSpec extends SpecBase with PageBehaviours {
 
     beRemovable[Int](ConsignorPage(index))
 
+    val consignee1Eori = arbitrary[GbEori].sample.value
+    val consignee2Eori = arbitrary[GbEori].sample.value
+    val consignee1Key = 1
+    val consignee2Key = 2
+    val notifiedParty1Eori = arbitrary[GbEori].sample.value
+    val notifiedParty2Eori = arbitrary[GbEori].sample.value
+    val notifiedParty1Key = 1
+    val notifiedParty2Key = 2
+
     "must navigate in Normal Mode" - {
 
-      "to Index" in {
+      "when there is one consignee and no notified parties" - {
 
-        ConsignorPage(index).navigate(NormalMode, emptyUserAnswers)
-          .mustEqual(routes.IndexController.onPageLoad)
+        "to wherever the Consignee page would navigate to" in {
+
+          val answers =
+            emptyUserAnswers
+              .set(ConsigneeKeyQuery(Index(0)), consignee1Key).success.value
+              .set(ConsigneeEORIPage(Index(0)), consignee1Eori).success.value
+
+          ConsignorPage(index).navigate(NormalMode, answers)
+            .mustEqual(ConsigneePage(index).navigate(NormalMode, answers))
+        }
+      }
+
+      "when there are no consignees and one notified party" - {
+
+        "to wherever the Notified Party page would navigate to" in {
+
+          val answers =
+            emptyUserAnswers
+              .set(NotifiedPartyKeyQuery(Index(0)), notifiedParty1Key).success.value
+              .set(NotifiedPartyEORIPage(Index(0)), notifiedParty1Eori).success.value
+
+          ConsignorPage(index).navigate(NormalMode, answers)
+            .mustEqual(NotifiedPartyPage(index).navigate(NormalMode, answers))
+        }
+      }
+
+      "when there is one consignee and one notified party" - {
+
+        "to Consignee Known" in {
+
+          val answers =
+            emptyUserAnswers
+              .set(ConsigneeKeyQuery(Index(0)), consignee1Key).success.value
+              .set(ConsigneeEORIPage(Index(0)), consignee1Eori).success.value
+              .set(NotifiedPartyKeyQuery(Index(0)), notifiedParty1Key).success.value
+              .set(NotifiedPartyEORIPage(Index(0)), notifiedParty1Eori).success.value
+
+          ConsignorPage(index).navigate(NormalMode, answers)
+            .mustEqual(goodsRoutes.ConsigneeKnownController.onPageLoad(NormalMode, answers.lrn, index))
+        }
+      }
+
+      "when there are multiple consignees and one notified party" - {
+
+        "to Consignee Known" in {
+
+          val answers =
+            emptyUserAnswers
+              .set(ConsigneeKeyQuery(Index(0)), consignee1Key).success.value
+              .set(ConsigneeEORIPage(Index(0)), consignee1Eori).success.value
+              .set(ConsigneeKeyQuery(Index(1)), consignee2Key).success.value
+              .set(ConsigneeEORIPage(Index(1)), consignee2Eori).success.value
+              .set(NotifiedPartyKeyQuery(Index(0)), notifiedParty1Key).success.value
+              .set(NotifiedPartyEORIPage(Index(0)), notifiedParty1Eori).success.value
+
+          ConsignorPage(index).navigate(NormalMode, answers)
+            .mustEqual(goodsRoutes.ConsigneeKnownController.onPageLoad(NormalMode, answers.lrn, index))
+        }
+      }
+
+      "when there is one consignee and multiple notified parties" - {
+
+        "to Consignee Known" in {
+
+          val answers =
+            emptyUserAnswers
+              .set(ConsigneeKeyQuery(Index(0)), consignee1Key).success.value
+              .set(ConsigneeEORIPage(Index(0)), consignee1Eori).success.value
+              .set(NotifiedPartyKeyQuery(Index(0)), notifiedParty1Key).success.value
+              .set(NotifiedPartyEORIPage(Index(0)), notifiedParty1Eori).success.value
+              .set(NotifiedPartyKeyQuery(Index(1)), notifiedParty2Key).success.value
+              .set(NotifiedPartyEORIPage(Index(1)), notifiedParty2Eori).success.value
+
+          ConsignorPage(index).navigate(NormalMode, answers)
+            .mustEqual(goodsRoutes.ConsigneeKnownController.onPageLoad(NormalMode, answers.lrn, index))
+        }
+      }
+
+      "when there are multiple consignees and no notified parties" - {
+
+        "to Consignee" in {
+
+          val answers =
+            emptyUserAnswers
+              .set(ConsigneeKeyQuery(Index(0)), consignee1Key).success.value
+              .set(ConsigneeEORIPage(Index(0)), consignee1Eori).success.value
+              .set(ConsigneeKeyQuery(Index(1)), consignee2Key).success.value
+              .set(ConsigneeEORIPage(Index(1)), consignee2Eori).success.value
+
+          ConsignorPage(index).navigate(NormalMode, answers)
+            .mustEqual(goodsRoutes.ConsigneeController.onPageLoad(NormalMode, answers.lrn, index))
+        }
+      }
+
+      "when there are no consignees and multiple notified parties" - {
+
+        "to Notified Party" in {
+
+          val answers =
+            emptyUserAnswers
+              .set(NotifiedPartyKeyQuery(Index(0)), notifiedParty1Key).success.value
+              .set(NotifiedPartyEORIPage(Index(0)), notifiedParty1Eori).success.value
+              .set(NotifiedPartyKeyQuery(Index(1)), notifiedParty2Key).success.value
+              .set(NotifiedPartyEORIPage(Index(1)), notifiedParty2Eori).success.value
+
+          ConsignorPage(index).navigate(NormalMode, answers)
+            .mustEqual(goodsRoutes.NotifiedPartyController.onPageLoad(NormalMode, answers.lrn, index))
+        }
       }
     }
 
