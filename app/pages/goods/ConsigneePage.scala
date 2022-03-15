8542863a4779bfbc97ece20ba8bd1707af856a64
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-03-15 10:42:37 +0000

    SSUT-291: Add navigation from the consignee page

--- app/pages/goods/ConsigneePage.scala
+++ app/pages/goods/ConsigneePage.scala
@@ -22,10 +22,18 @@ import models.{Index, NormalMode, UserAnswers}
 import pages.QuestionPage
 import play.api.libs.json.JsPath
 import play.api.mvc.Call
+import queries.routedetails.DeriveNumberOfPlacesOfLoading
 
 final case class ConsigneePage(itemIndex: Index) extends QuestionPage[Int] {
 
   override def path: JsPath = JsPath \ "goodsItems" \ itemIndex.position \ toString
 
   override def toString: String = "consigneeKey"
+
+  override protected def navigateInNormalMode(answers: UserAnswers): Call =
+    answers.get(DeriveNumberOfPlacesOfLoading).map {
+      n =>
+        if (n > 1) { goodsRoutes.LoadingPlaceController.onPageLoad(NormalMode, answers.lrn, itemIndex) }
+        else       { LoadingPlacePage(itemIndex).navigate(NormalMode, answers) }
+    }.getOrElse(routes.JourneyRecoveryController.onPageLoad())
 }
--- test/pages/goods/ConsigneePageSpec.scala
+++ test/pages/goods/ConsigneePageSpec.scala
@@ -19,8 +19,10 @@ package pages.goods
 import base.SpecBase
 import controllers.goods.{routes => goodsRoutes}
 import controllers.routes
-import models.{CheckMode, NormalMode}
+import models.{CheckMode, Index, NormalMode, PlaceOfLoading}
+import org.scalacheck.Arbitrary.arbitrary
 import pages.behaviours.PageBehaviours
+import pages.routedetails.PlaceOfLoadingPage
 
 class ConsigneeSpec extends SpecBase with PageBehaviours {
 
@@ -32,12 +34,30 @@ class ConsigneeSpec extends SpecBase with PageBehaviours {
 
     beRemovable[Int](ConsigneePage(index))
 
+    val placeOfLoading1 = arbitrary[PlaceOfLoading].sample.value
+    val placeOfLoading2 = arbitrary[PlaceOfLoading].sample.value
+
     "must navigate in Normal Mode" - {
 
-      "to Index" in {
+      "to place of loading when there is more than one place of loading" in {
 
-        ConsigneePage(index).navigate(NormalMode, emptyUserAnswers)
-          .mustEqual(routes.IndexController.onPageLoad)
+        val answers =
+          emptyUserAnswers
+            .set(PlaceOfLoadingPage(Index(0)), placeOfLoading1).success.value
+            .set(PlaceOfLoadingPage(Index(1)), placeOfLoading2).success.value
+
+        ConsigneePage(index).navigate(NormalMode, answers)
+          .mustEqual(goodsRoutes.LoadingPlaceController.onPageLoad(NormalMode, answers.lrn, index))
+      }
+
+      "to wherever loading place navigates to when there is one place of loading" in {
+
+        val answers =
+          emptyUserAnswers
+            .set(PlaceOfLoadingPage(Index(0)), placeOfLoading1).success.value
+
+        ConsigneePage(index).navigate(NormalMode, answers)
+          .mustEqual(LoadingPlacePage(Index(0)).navigate(NormalMode, answers))
       }
     }
 
