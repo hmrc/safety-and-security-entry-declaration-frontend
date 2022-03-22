64c21b7797dd53e8c60d4410d5e8252cc97ecddb
Juan Antonio Rodríguez Pozo <versatil5634@gmail.com>
2022-03-22 13:59:04 +0000

    Tasklist elements navigation (#85)
    * fixing navigation for consignors
    
    * fixing navigation from task list to notified parties and consignees

--- app/viewmodels/TaskListViewModel.scala
+++ app/viewmodels/TaskListViewModel.scala
@@ -21,9 +21,11 @@ import controllers.consignors.{routes => consignorRoutes}
 import controllers.predec.{routes => predecRoutes}
 import controllers.transport.{routes => transportRoutes}
 import controllers.routedetails.{routes => routedetailsRoutes}
-import models.{NormalMode, UserAnswers}
+import models.{Index, NormalMode, UserAnswers}
 import play.api.i18n.Messages
 import play.api.mvc.Call
+import queries.consignees.{DeriveNumberOfConsignees, DeriveNumberOfNotifiedParties}
+import queries.consignors.DeriveNumberOfConsignors
 import uk.gov.hmrc.govukfrontend.views.viewmodels.tag.Tag
 
 final case class TaskListViewModel(rows: Seq[TaskListRow])(implicit messages: Messages)
@@ -65,21 +67,36 @@ object TaskListViewModel {
       completionStatusTag = CompletionStatus.tag(CompletionStatus.NotStarted)
     )
 
-  private def consignorsRow(answers: UserAnswers)(implicit messages: Messages): TaskListRow =
+  private def consignorsRow(answers: UserAnswers)(implicit messages: Messages): TaskListRow = {
+    val url = answers.get(DeriveNumberOfConsignors) match {
+      case Some(size) if size > 0 => consignorRoutes.AddConsignorController.onPageLoad(NormalMode, answers.lrn)
+      case _ => consignorRoutes.ConsignorIdentityController.onPageLoad(NormalMode, answers.lrn, Index(0))
+    }
+
     TaskListRow(
       messageKey          = messages("taskList.consignors"),
-      link                = consignorRoutes.AddConsignorController.onPageLoad(NormalMode, answers.lrn),
+      link                = url,
       id                  = "consignors",
       completionStatusTag = CompletionStatus.tag(CompletionStatus.NotStarted)
     )
+  }
 
-  private def consigneesRow(answers: UserAnswers)(implicit messages: Messages): TaskListRow =
+  private def consigneesRow(answers: UserAnswers)(implicit messages: Messages): TaskListRow = {
+    val url = answers.get(DeriveNumberOfConsignees) match {
+      case Some(size) if size > 0 => consigneesRoutes.CheckConsigneesAndNotifiedPartiesController.onPageLoad(answers.lrn)
+      case _ => answers.get(DeriveNumberOfNotifiedParties) match {
+          case Some(size) if size > 0 => consigneesRoutes.CheckConsigneesAndNotifiedPartiesController.onPageLoad(answers.lrn)
+          case _ => consigneesRoutes.AnyConsigneesKnownController.onPageLoad(NormalMode,answers.lrn)
+        }
+    }
+
     TaskListRow(
       messageKey          = messages("taskList.consignees"),
-      link                = consigneesRoutes.AnyConsigneesKnownController.onPageLoad(NormalMode, answers.lrn),
+      link                = url,
       id                  = "consignees",
       completionStatusTag = CompletionStatus.tag(CompletionStatus.NotStarted)
     )
+  }
 }
 
 final case class TaskListRow(messageKey: String, link: Call, id: String, completionStatusTag: Tag)(
