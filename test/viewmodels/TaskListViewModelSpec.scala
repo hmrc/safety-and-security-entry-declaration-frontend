227b184352eb3eb0682b37ff8f2ca08abe72d626
Juan Antonio Rodríguez Pozo <versatil5634@gmail.com>
2022-05-05 08:51:24 +0100

    Tasklist predec status (#133)
    * starting to test drive behaviour
    
    * driving behaviour for PREDEC
    
    * refactor
    
    * fixing lrn
    
    * fixing test on missing LRN
    
    * refactor
    
    * PR comments

--- test/viewmodels/TaskListViewModelSpec.scala
+++ test/viewmodels/TaskListViewModelSpec.scala
@@ -17,10 +17,12 @@
 package viewmodels
 
 import base.SpecBase
+import controllers.predec.{routes => predecRoutes}
 import controllers.consignees.{routes => consigneeRoutes}
 import controllers.consignors.{routes => consignorRoutes}
 import controllers.goods.{routes => goodsRoutes}
-import models.{GbEori, Index}
+import models.{Address, GbEori, Index, LocalReferenceNumber, LodgingPersonType, ProvideGrossWeight, TraderIdentity, UserAnswers}
+import org.scalacheck.Arbitrary.arbitrary
 import org.scalatest.OptionValues
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
@@ -29,6 +31,7 @@ import pages.EmptyWaypoints
 import pages.consignees.{ConsigneeEORIPage, NotifiedPartyEORIPage}
 import pages.consignors.ConsignorEORIPage
 import pages.goods.CommodityCodeKnownPage
+import pages.predec.{CarrierEORIPage, CarrierIdentityPage, DeclarationPlacePage, LocalReferenceNumberPage, LodgingPersonTypePage, ProvideGrossWeightPage, TotalGrossWeightPage}
 import queries.consignees.{ConsigneeKeyQuery, NotifiedPartyKeyQuery}
 import queries.consignors.ConsignorKeyQuery
 
@@ -41,28 +44,95 @@ class TaskListViewModelSpec
 
   "On the task list" - {
     val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+    val predecIdx = 0
     val consignorsIdx = 3
     val consigneesIdx = 4
     val goodsIdx = 5
+    val location = "test-declaration-location"
+    val totalMass = 1000
+    val carrierEORI = arbitrary[GbEori].sample.value
+
+    "For the predec section" - {
+      "When we have a completed section" - {
+        "It will show a `complete` status and link to CYA" in {
+          val validAnswers = {
+            arbitrary[UserAnswers].sample.value.set(DeclarationPlacePage, location).success.value
+              .set(ProvideGrossWeightPage, ProvideGrossWeight.Overall).success.value
+              .set(TotalGrossWeightPage, BigDecimal.exact(totalMass)).success.value
+              .set(LodgingPersonTypePage, LodgingPersonType.Representative).success.value
+              .set(CarrierIdentityPage, TraderIdentity.GBEORI).success.value
+              .set(CarrierEORIPage, carrierEORI).success.value
+          }
+
+          val result = TaskListViewModel.fromAnswers(validAnswers)(messages(application))
+
+          result.rows(predecIdx).completionStatusTag mustEqual CompletionStatus.tag(
+            CompletionStatus.Completed
+          )(messages(application))
+
+          result.rows(predecIdx).link mustEqual predecRoutes.CheckPredecController.onPageLoad(
+            EmptyWaypoints,
+            validAnswers.lrn
+          )
+        }
+      }
+
+      "When we don't have a completed section" - {
+        "And the first question is populated" - {
+          "It will show an `in progress` status and link to first answer" in {
+            val validAnswers = emptyUserAnswers.set(DeclarationPlacePage,"Newcastle").success.value
+
+            val result = TaskListViewModel.fromAnswers(validAnswers)(messages(application))
+
+            result.rows(predecIdx).completionStatusTag mustEqual CompletionStatus.tag(
+              CompletionStatus.InProgress
+            )(messages(application))
+
+            result.rows(predecIdx).link mustEqual predecRoutes.LocalReferenceNumberController.onPageLoad()
+          }
+        }
+
+        "And the first question is not populated" - {
+          "It will show an `not started` status and link to first answer" in {
+            val result = TaskListViewModel.fromAnswers(emptyUserAnswers)(messages(application))
+
+            result.rows(predecIdx).completionStatusTag mustEqual CompletionStatus.tag(
+              CompletionStatus.NotStarted
+            )(messages(application))
+
+            result.rows(predecIdx).link mustEqual predecRoutes.LocalReferenceNumberController.onPageLoad()
+          }
+        }
+      }
+    }
 
     "For the consignors section" - {
       "When we already have some" - {
         "we go to the consignors listing page" in {
           val answers =
             emptyUserAnswers
-              .set(ConsignorEORIPage(Index(0)), GbEori("123456789000")).success.value
-              .set(ConsignorKeyQuery(Index(0)), 1).success.value
+              .set(ConsignorEORIPage(Index(0)), GbEori("123456789000"))
+              .success
+              .value
+              .set(ConsignorKeyQuery(Index(0)), 1)
+              .success
+              .value
 
           val result = TaskListViewModel.fromAnswers(answers)(messages(application))
 
-          result.rows(consignorsIdx).link mustEqual consignorRoutes.AddConsignorController.onPageLoad(EmptyWaypoints,answers.lrn)
+          result.rows(consignorsIdx).link mustEqual consignorRoutes.AddConsignorController
+            .onPageLoad(EmptyWaypoints, answers.lrn)
         }
       }
       "When have don't have any" - {
         "we go to the first consignor input" in {
           val result = TaskListViewModel.fromAnswers(emptyUserAnswers)(messages(application))
 
-          result.rows(consignorsIdx).link mustEqual consignorRoutes.ConsignorIdentityController.onPageLoad(EmptyWaypoints,emptyUserAnswers.lrn,Index(0))
+          result.rows(consignorsIdx).link mustEqual consignorRoutes.ConsignorIdentityController.onPageLoad(
+            EmptyWaypoints,
+            emptyUserAnswers.lrn,
+            Index(0)
+          )
         }
       }
     }
@@ -72,13 +142,17 @@ class TaskListViewModelSpec
         "we go to the consignees and notified parties check page" in {
           val answers =
             emptyUserAnswers
-              .set(ConsigneeEORIPage(Index(0)), GbEori("123456789000")).success.value
-              .set(ConsigneeKeyQuery(Index(0)), 1).success.value
-
+              .set(ConsigneeEORIPage(Index(0)), GbEori("123456789000"))
+              .success
+              .value
+              .set(ConsigneeKeyQuery(Index(0)), 1)
+              .success
+              .value
 
           val result = TaskListViewModel.fromAnswers(answers)(messages(application))
 
-          result.rows(consigneesIdx).link mustEqual consigneeRoutes.CheckConsigneesAndNotifiedPartiesController.onPageLoad(EmptyWaypoints, answers.lrn)
+          result.rows(consigneesIdx).link mustEqual consigneeRoutes.CheckConsigneesAndNotifiedPartiesController
+            .onPageLoad(EmptyWaypoints, answers.lrn)
         }
       }
 
@@ -86,13 +160,17 @@ class TaskListViewModelSpec
         "we go to the consignees and notified parties check page" in {
           val answers =
             emptyUserAnswers
-              .set(NotifiedPartyEORIPage(Index(0)), GbEori("123456789000")).success.value
-              .set(NotifiedPartyKeyQuery(Index(0)), 1).success.value
-
+              .set(NotifiedPartyEORIPage(Index(0)), GbEori("123456789000"))
+              .success
+              .value
+              .set(NotifiedPartyKeyQuery(Index(0)), 1)
+              .success
+              .value
 
           val result = TaskListViewModel.fromAnswers(answers)(messages(application))
 
-          result.rows(consigneesIdx).link mustEqual consigneeRoutes.CheckConsigneesAndNotifiedPartiesController.onPageLoad(EmptyWaypoints, answers.lrn)
+          result.rows(consigneesIdx).link mustEqual consigneeRoutes.CheckConsigneesAndNotifiedPartiesController
+            .onPageLoad(EmptyWaypoints, answers.lrn)
         }
       }
 
@@ -100,7 +178,8 @@ class TaskListViewModelSpec
         "we go to the beginning of the journey for consignees and notified parties" in {
           val result = TaskListViewModel.fromAnswers(emptyUserAnswers)(messages(application))
 
-          result.rows(consigneesIdx).link mustEqual consigneeRoutes.AnyConsigneesKnownController.onPageLoad(EmptyWaypoints, emptyUserAnswers.lrn)
+          result.rows(consigneesIdx).link mustEqual consigneeRoutes.AnyConsigneesKnownController
+            .onPageLoad(EmptyWaypoints, emptyUserAnswers.lrn)
         }
       }
     }
@@ -110,18 +189,24 @@ class TaskListViewModelSpec
         "we go to the goods listing page" in {
           val answers =
             emptyUserAnswers
-              .set(CommodityCodeKnownPage(Index(0)), true).success.value
+              .set(CommodityCodeKnownPage(Index(0)), true)
+              .success
+              .value
 
           val result = TaskListViewModel.fromAnswers(answers)(messages(application))
 
-          result.rows(goodsIdx).link mustEqual goodsRoutes.AddGoodsController.onPageLoad(EmptyWaypoints,answers.lrn)
+          result.rows(goodsIdx).link mustEqual goodsRoutes.AddGoodsController.onPageLoad(EmptyWaypoints, answers.lrn)
         }
       }
       "When have don't have any" - {
         "we go to the first good input" in {
           val result = TaskListViewModel.fromAnswers(emptyUserAnswers)(messages(application))
 
-          result.rows(goodsIdx).link mustEqual goodsRoutes.InitialiseGoodsItemController.initialise(EmptyWaypoints, emptyUserAnswers.lrn, Index(0))
+          result.rows(goodsIdx).link mustEqual goodsRoutes.InitialiseGoodsItemController.initialise(
+            EmptyWaypoints,
+            emptyUserAnswers.lrn,
+            Index(0)
+          )
         }
       }
     }
